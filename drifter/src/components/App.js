00d26e8d676c64d0c51a357c653db01b1d0799fa
Silas Nevstad <115965493+silasnevstad@users.noreply.github.com>
2023-04-30 21:46:56 -0400

    Initial Commit

---
+++ drifter/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DriftGame from './DriftGame';
+
+function App() {
+  return (
+    <div className="App">
+      <DriftGame />
+    </div>
+  );
+}
+
+export default App;
---
+++ drifter/src/components/InputHandler.js
@@ -0,0 +1,40 @@
+class InputHandler {
+    constructor(car) {
+      this.car = car;
+      this.pressedKeys = {};
+  
+      // Bind the event handlers to the instance
+      this.onKeyDown = this.onKeyDown.bind(this);
+      this.onKeyUp = this.onKeyUp.bind(this);
+  
+      // Add event listeners for key presses and releases
+      window.addEventListener('keydown', this.onKeyDown);
+      window.addEventListener('keyup', this.onKeyUp);
+    }
+  
+    onKeyDown(event) {
+      this.pressedKeys[event.code] = true;
+    }
+  
+    onKeyUp(event) {
+      delete this.pressedKeys[event.code];
+    }
+  
+    update() {
+      if (this.pressedKeys['ArrowUp']) {
+        this.car.accelerate();
+      }
+      if (this.pressedKeys['ArrowDown']) {
+        this.car.brake();
+      }
+      if (this.pressedKeys['ArrowLeft']) {
+        this.car.turn(1);
+      }
+      if (this.pressedKeys['ArrowRight']) {
+        this.car.turn(-1);
+      }
+    }
+  }
+  
+  export default InputHandler;
+  
